1c9cdf33b6a93664f857246b0db0062dab8ba16e
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2024-09-09 11:29:56 +0200

    refactor: move typer subcommands into each module

--- src/ai4_metadata/generate.py
+++ src/ai4_metadata/generate.py
@@ -3,13 +3,21 @@
 import collections
 import json
 import pathlib
-import typing
+from typing_extensions import Annotated, Optional
+from typing import Union, Any
 
+import typer
+
+import ai4_metadata
 from ai4_metadata import exceptions
+from ai4_metadata import utils
+from ai4_metadata import validate
+
+app = typer.Typer()
 
 
 def generate(
-    schema: typing.Union[pathlib.Path, str],
+    schema: Union[pathlib.Path, str],
     sample_values: bool = False,
     required_only: bool = False,
 ) -> collections.OrderedDict:
@@ -27,7 +35,7 @@ def generate(
             schema, "No definitions found in the schema."
         )
 
-    generated_json: collections.OrderedDict[str, typing.Any] = collections.OrderedDict()
+    generated_json: collections.OrderedDict[str, Any] = collections.OrderedDict()
 
     version = properties.pop("metadata_version").get("example")
     generated_json["metadata_version"] = version
@@ -38,7 +46,7 @@ def generate(
     return generated_json
 
 
-def _get_field_value(value: dict, sample_values: bool = False) -> typing.Any:
+def _get_field_value(value: dict, sample_values: bool = False) -> Any:
     """Get the value of a field."""
     if value.get("type") == "object":
         required = value.get("required", [])
@@ -61,3 +69,39 @@ def _get_field_value(value: dict, sample_values: bool = False) -> typing.Any:
             return value.get("example", "")
         else:
             return ""
+
+
+@app.command(name="generate")
+def main(
+    metadata_version: Annotated[
+        ai4_metadata.MetadataVersions,
+        typer.Option(help="AI4 application metadata version."),
+    ] = ai4_metadata.get_latest_version(),
+    sample_values: Annotated[
+        bool, typer.Option("--sample-values", help="Generate sample values.")
+    ] = False,
+    required: Annotated[
+        bool, typer.Option("--required-only", help="Include only required fields.")
+    ] = False,
+    output: Annotated[
+        Optional[pathlib.Path],
+        typer.Option("--output", "-o", help="Output file for generated metadata."),
+    ] = None,
+):
+    """Generate an AI4 metadata schema."""
+    schema = ai4_metadata.get_schema(metadata_version)
+
+    try:
+        generated_json = generate(schema, sample_values, required)
+    except exceptions.InvalidSchemaError as e:
+        utils.format_rich_error(e)
+        raise typer.Exit(1)
+
+    utils.dump_json(generated_json, output)
+
+    validate.validate(generated_json, schema)
+
+    if output:
+        utils.format_rich_ok(
+            f"Sample file stored in '{output}' for version {metadata_version.value}"
+        )
